862b8cfa06f31c7176caf29fc737fdf0a86b7fae
“Dorokhin <ekonom82@inbox.ru>
2024-05-20 01:15:50 +0300

    modified CreateNewProjectTest

--- src/test/java/com/example/teamcity/ui/CreateNewProjectTest.java
+++ src/test/java/com/example/teamcity/ui/CreateNewProjectTest.java
@@ -1,31 +1,33 @@
 package com.example.teamcity.ui;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.selector.ByAttribute;
-import com.example.teamcity.api.requests.checked.CheckedUser;
-import com.example.teamcity.api.spec.Specifications;
+import com.codeborne.selenide.Condition;
+import com.example.teamcity.ui.pages.favorites.ProjectsPage;
+import com.example.teamcity.ui.pages.admin.CreateNewProject;
 import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Selenide.element;
 
 public class CreateNewProjectTest extends BaseUiTest {
     @Test
     public void authorizedUserShouldBeAbleCreateNewProject() {
         var testData = testDataStorage.addTestData();
+        // link to your repository necessary to fill textfield during creation new project
+        var url = "https://github.com/ekonom82/psheacademy_teamcity";
 
-        new CheckedUser(Specifications.getSpec().superUserSpec())
-                .create(testData.getUser());
+        // данная функциональность будет использоваться постоянно, поэтому ее вынесли в метод loginAsUser() класса BaseUiTest
+//        new LoginPage().open().login(testData.getUser());
+        loginAsUser(testData.getUser());
 
-        // не нужно указывать полный путь "http://localhost:8111/login.html", так как ХОСТ (baseUrl) уже указан в BaseUiTest
-        Selenide.open("/login.html");
+        new CreateNewProject()
+                .open(testData.getProject().getParentProject().getLocator())
+                .createProjectByUrl(url)
+                .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
 
-        var usernameInput = element(new ByAttribute("id", "username"));
-        var passwordInput = element(new ByAttribute("id", "password"));
-        var logInButton = element(new ByAttribute("type", "submit"));
-
-        usernameInput.sendKeys(testData.getUser().getUsername());
-        passwordInput.sendKeys(testData.getUser().getPassword());
-
-        logInButton.click();
+        // после создания проекта он окажется самым нижним в списке на странице
+        // чтобы его получить, можно весь список с проектами схлопнуть до двух с помощью stream и его конструкции
+        // .stream().reduce((first, second) -> second).get()
+        // и далее работать с ним: получаем его хедер и проверяем или он соответствует сгенерированным данным проекта, используемые при его создании
+        new ProjectsPage().open()
+                .getSubprojects()
+                .stream().reduce((first, second) -> second).get()
+                .getHeader().shouldHave(Condition.text(testData.getProject().getName()));
     }
 }
