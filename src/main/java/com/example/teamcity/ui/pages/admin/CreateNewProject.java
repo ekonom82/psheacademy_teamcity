d8f9a431bfd13191d72589bf4eae1accae4ec6bd
“Dorokhin <ekonom82@inbox.ru>
2024-05-20 15:25:09 +0300

    modified createProject by adding createProject()

--- src/main/java/com/example/teamcity/ui/pages/admin/CreateNewProject.java
+++ src/main/java/com/example/teamcity/ui/pages/admin/CreateNewProject.java
@@ -4,16 +4,19 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.example.teamcity.ui.Selectors;
 import com.example.teamcity.ui.pages.Page;
+import lombok.Getter;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.element;
 
+@Getter
 public class CreateNewProject extends Page {
     private final SelenideElement urlInput = element(Selectors.byId("url"));
     private final SelenideElement projectNameInput = element(Selectors.byId("projectName"));
     private final SelenideElement buildTypeNameInput = element(Selectors.byId("buildTypeName"));
+    private final SelenideElement errorProjectNameText = element(Selectors.byId("error_projectName"));
 
     public CreateNewProject open(String parentProjectId) {
         // that link we see on Create New Project page when we click on "+" to create new project
@@ -36,5 +39,11 @@ public class CreateNewProject extends Page {
         buildTypeNameInput.clear();
         buildTypeNameInput.sendKeys(buildTypeName);
         submit();
+    }
+
+    public void createProject(String locator, String urlRepo, String projectName, String buildTypeName) {
+        open(locator);
+        createProjectByUrl(urlRepo);
+        setupProject(projectName, buildTypeName);
     }
 }
