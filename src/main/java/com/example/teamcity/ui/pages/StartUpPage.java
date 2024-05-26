fd4ecb81ee9b876f9a08828c81c67f85ee5645aa
“Dorokhin <ekonom82@inbox.ru>
2024-05-26 22:17:42 +0300

    updated StartUpPage

--- src/main/java/com/example/teamcity/ui/pages/StartUpPage.java
+++ src/main/java/com/example/teamcity/ui/pages/StartUpPage.java
@@ -1,26 +1,24 @@
-package com.example.teamcity.ui.pages.admin;
+package com.example.teamcity.ui.pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.example.teamcity.ui.Selectors;
-import com.example.teamcity.ui.pages.Page;
-import lombok.Getter;
 import org.openqa.selenium.By;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.element;
 
-@Getter
 public class StartUpPage extends Page {
+    private static final String START_UP_PAGE_URL = "/";
+
     private final SelenideElement proceedButton = element(Selectors.byId("proceedButton"));
     private final SelenideElement continueButton = element(By.xpath("//*[@name='Continue']"));
-    private final SelenideElement headerTitle = element(Selectors.byId("header"));
     private final SelenideElement acceptLicenseCheckBox = element(Selectors.byId("accept"));
 
     public StartUpPage open() {
-        Selenide.open("/");
+        Selenide.open(START_UP_PAGE_URL);
         return this;
     }
 
