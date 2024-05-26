abb88a1580c59486e218d051a52050ea5e51ac62
“Dorokhin <ekonom82@inbox.ru>
2024-05-27 00:21:14 +0300

    updated SetupFirstStartTest by adding setupTeamCityAgentTest()

--- src/test/java/com/example/teamcity/ui/SetupFirstStartTest.java
+++ src/test/java/com/example/teamcity/ui/SetupFirstStartTest.java
@@ -1,9 +1,13 @@
 package com.example.teamcity.ui;
 
 import com.codeborne.selenide.Condition;
+import com.example.teamcity.ui.pages.AgentsPage;
 import com.example.teamcity.ui.pages.LoginAsSuperUserPage;
 import com.example.teamcity.ui.pages.SetUpAdminPage;
 import com.example.teamcity.ui.pages.StartUpPage;
+import com.example.teamcity.ui.pages.UnauthorizedAgentsPage;
+import com.example.teamcity.ui.pages.favorites.ProjectsPage;
+import com.example.teamcity.ui.popups.AuthorizeUnauthorizedAgentPopup;
 import org.testng.annotations.Test;
 
 public class SetupFirstStartTest extends BaseUiTest {
@@ -27,5 +31,18 @@ public class SetupFirstStartTest extends BaseUiTest {
         new LoginAsSuperUserPage()
                 .open()
                 .login();
+        new ProjectsPage()
+                .open()
+                .clickAgentsMenuButton();
+        new AgentsPage()
+                .open()
+                .clickUnauthorizedAgentsButton();
+        new UnauthorizedAgentsPage()
+                .open()
+                .clickAuthorizeButton();
+        new AuthorizeUnauthorizedAgentPopup().clickAuthorizeButton();
+        new AgentsPage()
+                .open()
+                .getAgentsNumberText().shouldHave(Condition.text("1"));
     }
 }
